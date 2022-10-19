4c81580ce07eadac77616f022d7cc62d5e690096
Erick Milan <erickmilan1002@gmail.com>
2022-10-18 21:55:37 -0500

    add qr model and its configurations

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanButton extends StatelessWidget {
   const ScanButton({super.key});
@@ -6,8 +7,13 @@ class ScanButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
+      elevation: 0,
       child: const Icon(Icons.filter_center_focus_rounded),
-      onPressed: () {},
+      onPressed: () async {
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+            '#3D8BEF', 'Cancel', true, ScanMode.QR);
+        print(barcodeScanRes);
+      },
     );
   }
 }
