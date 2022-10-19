bd509fdb3348740d6f46f6659f8e7e3861acc6b9
Erick Milan <erickmilan1002@gmail.com>
2022-10-18 20:43:30 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:qrscann_flutter/screens/home_page.dart';
+import 'package:qrscann_flutter/screens/map_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'QR scann',
+      initialRoute: 'home',
+      routes: {'home': (_) => const HomePage(), 'map': (_) => const MapPage()},
+    );
+  }
+}
